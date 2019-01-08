da5a3966ec15d69b09018636fbe9c67beed26ddf
asim <asim.k@aceturtle.com>
2019-01-08 15:02:48 +0530

    Db connection file

--- playground/mongo-db-connect.js
+++ playground/mongo-db-connect.js
@@ -1,48 +1,10 @@
-// var user = {name:'asim',lname:'Katamale'};
-// var {name} = user;
-// console.log(name);
-
-
-const Mongoclient = require('mongodb').MongoClient;
-Mongoclient.connect('mongodb://localhost/27017/TodoApp',{
-  useNewUrlParser : true
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost/27017/TodoApp',{
+  useNewUrlParser:true
 },(err,client)=>{
   if(err){
-    console.log('Could not connect to mongodb');
+    return console.log(err);
   }
   console.log('Connected to server');
-  const db = client.db('TodoApp');
-  // db.collection('Products').insertOne({
-  //   id:"1",
-  //   product_name:"Shirt"
-  // },(err,result)=>{
-  //   if(err){
-  //     return console.log('Unable to insert user' +err);
-  //   }
-  //   console.log(result.ops);
-  // });
-
-
-  db.collection('Products').insertMany(
-    [
-      {
-        id:'4',
-        name:'Product Name4'
-      },
-      {
-        id:'5',
-        name:'Product Name5'
-      },
-      {
-        id:'6',
-        name:'Product Name6'
-      }
-    ],(err,result)=>{
-      if(err){
-        return console.log('Unable to insert products',err);
-      }
-      console.log(JSON.stringify(result,undefined,2));
-    }
-  )
   client.close();
 });
